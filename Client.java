55c9aa432eac44ceca8df20a02417b1569453f0d
J4RD0 <jarodholgado@gmail.com>
2021-05-02 19:12:19 -0400

    added Terminal client with minimal UI

--- Client.java
+++ Client.java
@@ -1,5 +1,6 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class Client {
 
@@ -11,8 +12,31 @@ public class Client {
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             GradeInquiry stub = (GradeInquiry) registry.lookup("GradeInquiry");
-            Double response = stub.studentGrade("Jarod Holgado");
-            System.out.println("response: " + response);
+            boolean run = true;
+            Scanner scan = new Scanner(System.in);
+            String input;
+            while(run){
+                System.out.println("Welcome to Grade Inquiry! Please select and operation:");
+                System.out.println("G - get grade");
+                System.out.println("Q - quit");
+                input = scan.nextLine().toUpperCase();
+                switch(input){
+                    case "G":
+                        System.out.println("Please enter a student name");
+                        input = scan.nextLine();
+                        Double response = stub.studentGrade(input);
+                        System.out.println("Grade: " + response);
+                        break;
+                    case "Q":
+                        System.out.println("exiting client");
+                        run = false;
+                        break;
+                    default:
+                        System.out.println("invalid command");
+                        break;
+                }
+            }
+            scan.close();
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
